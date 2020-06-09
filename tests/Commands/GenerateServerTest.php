4cb6cdf86dcdf9efd4bd0d348b5cb4678b86f9c4
Nikita Kutselay <nikit.kutselaj2013@yandex.ru>
2020-06-09 17:51:57 +0300

    Fix test and add base docs

--- tests/Commands/GenerateServerTest.php
+++ tests/Commands/GenerateServerTest.php
@@ -12,8 +12,13 @@ class GenerateServerTest extends TestCase
     }
 
     protected function getEnvironmentSetUp($app): void {
-        $app['config']->set('openapi-server-generator.output_dir', './gen-test');
-        $app['config']->set('openapi-server-generator.app_dir', './app-test/OpenApiGenerated');
+        $app['config']->set('openapi-server-generator.apidoc_dir', ('./public/api-docs'));
+        $app['config']->set('openapi-server-generator.output_dir', './generated');
+        $app['config']->set('openapi-server-generator.app_dir', 'OpenApiGenerated');
+        $app['config']->set(
+            'openapi-server-generator.openapi_generator_bin',
+            './node_modules/.bin/openapi-generator'
+        );
     }
 
     protected function getPackageProviders($app)
