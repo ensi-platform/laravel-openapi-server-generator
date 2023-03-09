80b82e94cb993908faf3f543c2a22ed135406ca1
mtyrtov <mtyrtov@users.noreply.github.com>
2023-03-09 03:59:36 +0000

    Fix styling

--- tests/PolicyGenerationTest.php
+++ tests/PolicyGenerationTest.php
@@ -1,12 +1,6 @@
 <?php
 
-use Ensi\LaravelOpenApiServerGenerator\Commands\GenerateServer;
-use Ensi\LaravelOpenApiServerGenerator\Tests\TestCase;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Config;
 
-use function Pest\Laravel\artisan;
-use function PHPUnit\Framework\assertEqualsCanonicalizing;
 
 test('can generate policy', function () {
     \PHPUnit\Framework\assertIsBool(true);
